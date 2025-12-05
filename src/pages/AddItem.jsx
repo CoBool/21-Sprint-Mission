695b5875f56ffae889203d210692ea8bfd03a0d4
cobool <saokm5@gmail.com>
2025-12-06 05:10:03 +0900

    feat: AddItem 페이지 폼 구조 및 스타일 개선
    - AddItem 컴포넌트의 폼 구조를 개선하여 헤더와 제출 버튼 추가
    - 각 입력 필드에 대한 레이블을 추가하고, id 속성을 설정하여 접근성 향상
    - CSS 모듈에 새로운 스타일 클래스 추가하여 레이아웃 및 디자인 개선

--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -7,30 +7,62 @@ export default function AddItem() {
   return (
     <main>
       <div className={`container ${styles["addItem__container"]}`}>
-        <section className={`${styles["addItems__section"]}`}>
-          <h1 className={`${styles["addItems__section__title"]}`}>
-            상품 등록하기
-          </h1>
-          <form action={`${styles["addItems__section__form"]}`}>
-            <div className={`${styles["addItems__section__form--group"]}`}>
-              <label>상품 이미지</label>
-              <input type="file" />
+        <section className={`${styles["addItem__section"]}`}>
+          <form className={`${styles["addItem__section__form"]}`}>
+            <div className={`${styles["addItem__section__form--header"]}`}>
+              <h1 className={`${styles["addItem__section__form--header__title"]}`}>
+                상품 등록하기
+              </h1>
+
+              <button type="submit" className={`${styles["addItem__section__form--header__button"]}`}>
+                등록
+              </button>
             </div>
-            <div className={`${styles["addItems__section__form--group"]}`}>
-              <label>상품명</label>
-              <input type="text" />
+
+            <div className={`${styles["addItem__section__form--group"]}`}>
+              <label
+                htmlFor="image"
+                className={`${styles["addItem__section__form--group__label"]}`}
+              >
+                상품 이미지
+              </label>
+              <input type="file" id="image" />
             </div>
-            <div className={`${styles["addItems__section__form--group"]}`}>
-              <label>상품 소개</label>
-              <textarea></textarea>
+            <div className={`${styles["addItem__section__form--group"]}`}>
+              <label
+                htmlFor="name"
+                className={`${styles["addItem__section__form--group__label"]}`}
+              >
+                상품명
+              </label>
+              <input type="text" id="name" />
             </div>
-            <div className={`${styles["addItems__section__form--group"]}`}>
-              <label>판매가격</label>
-              <input type="number" />
+            <div className={`${styles["addItem__section__form--group"]}`}>
+              <label
+                htmlFor="description"
+                className={`${styles["addItem__section__form--group__label"]}`}
+              >
+                상품 소개
+              </label>
+              <textarea id="description"></textarea>
             </div>
-            <div className={`${styles["addItems__section__form--group"]}`}>
-              <label>태그</label>
-              <input type="text" />
+            <div className={`${styles["addItem__section__form--group"]}`}>
+              <label
+                htmlFor="price"
+                className={`${styles["addItem__section__form--group__label"]}`}
+              >
+                판매가격
+              </label>
+              <input type="number" id="price" />
+            </div>
+            <div className={`${styles["addItem__section__form--group"]}`}>
+              <label
+                htmlFor="tags"
+                className={`${styles["addItem__section__form--group__label"]}`}
+              >
+                태그
+              </label>
+              <input type="text" id="tags" />
             </div>
           </form>
         </section>
