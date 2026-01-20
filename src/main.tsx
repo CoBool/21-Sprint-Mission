ce3a54fe55e98c069f31a22d246ef16a7593a854
cobool <saokm5@gmail.com>
2026-01-20 16:15:37 +0900

    feat: update language to Korean, add Tailwind CSS import, and implement basic routing with Home component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import './index.css'
+
+import { Home } from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>판다마켓</h1>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <main>
+      <section>
+        히어로섹션 들어가는곳...
+      </section>
+
+      <section>
+        <div>카드1</div>
+        <div>카드2</div>
+        <div>카드3</div>
+      </section>
+
+      <section>
+        히어로섹션 들어가는곳...
+      </section>
+    </main>
+  )
+}
+
+export { Home }
