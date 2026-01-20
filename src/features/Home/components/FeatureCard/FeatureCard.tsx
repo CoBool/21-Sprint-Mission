a11a6d7b9edea7501ec57e44ed57d5feb59d9c96
배상빈 <saokm5@gmail.com>
2026-01-20 20:39:16 +0900

    feater: Home 기본틀 작업

---
+++ src/features/Home/components/FeatureCard/FeatureCard.tsx
@@ -0,0 +1,11 @@
+// import styles from './FeatureCard.module.css';
+
+function FeatureCard() {
+  return (
+    <section>
+      <div>카드1</div>
+    </section>
+  );
+}
+
+export { FeatureCard }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,24 @@
+import { Link } from 'react-router';
+
+import styles from './Home.module.css';
+
 function Home() {
   return (
-    <main>
-      <section>
-        히어로섹션 들어가는곳...
+    <main >
+      <section className={`${styles.Hero} ${styles.HeroFirst}`}>
+        <div className={`layout-container`}>
+          <div className={`${styles.HeroContainer}`}>
+            <div className={`${styles.HeroText}`}>
+              <h1 className={`${styles.HeroTitle}`}>
+                일상의 모든 물건을 거래해보세요
+              </h1>
+              <Link to="/items" className={`${styles.HeroButton}`}>
+                구경하러 가기
+              </Link>
+            </div>
+            <div className={styles.HeroImage}></div>
+          </div>
+        </div>
       </section>
 
       <section>
@@ -11,8 +27,17 @@ function Home() {
         <div>카드3</div>
       </section>
 
-      <section>
-        히어로섹션 들어가는곳...
+      <section className={`${styles.Hero} ${styles.HeroSecond}`}>
+        <div className={`layout-container`}>
+          <div className={`${styles.HeroContainer}`}>
+            <div className={`${styles.HeroText}`}>
+              <h1 className={`${styles.HeroTitle}`}>
+                믿을 수 있는 판다마켓 중고 거래
+              </h1>
+            </div>
+            <div className={styles.HeroImage}></div>
+          </div>
+        </div>
       </section>
     </main>
   )
